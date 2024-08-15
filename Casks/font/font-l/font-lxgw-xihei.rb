27f8a51dbd6741bfc806e378d821c8d140a3d29e
Lex.Chen <ctyytc@gmail.com>
2024-08-15 18:24:15 +0800

    Add font-lxgw-xihei 1.200

---
+++ Casks/font/font-l/font-lxgw-xihei.rb
@@ -0,0 +1,13 @@
+cask "font-lxgw-xihei" do
+  version "1.200"
+  sha256 "f26fb5f6524d760fa606fbc702283c37267800e0b6344de4eeb965eeedf672ec"
+
+  url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
+  name "LXGW NeoXiHei"
+  name "霞鹜新晰黑"
+  homepage "https://github.com/lxgw/LxgwNeoXiHei"
+
+  font "LXGWNeoXiHei.ttf"
+
+  # No zap stanza required
+end
